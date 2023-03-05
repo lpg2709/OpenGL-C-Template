b0fe8aed5546220ce011b16f94dfa6ffa402491b
lpg2709 <leonardo_guarezi@hotmail.com>
2023-03-05 15:51:26 -0300

    Update main.c

--- src/Sources/main.c
+++ src/Sources/main.c
@@ -11,24 +11,35 @@
 int main(int argc, char * argv[]) {
 
     // Load GLFW and Create a Window
-    glfwInit();
+    if(!glfwInit()){
+        printf("Failt to init GLFW!");
+        return EXIT_FAILURE;
+    }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    GLFWwindow  *mWindow = glfwCreateWindow(mWidth, mHeight, "opengl-c-template", NULL, NULL);
 
+    GLFWwindow  *mWindow = glfwCreateWindow(mWidth, mHeight, "opengl-c-template", NULL, NULL);
 
     // Check for Valid Context
     if (mWindow == NULL) {
-        fprintf(stderr, "Failed to Create OpenGL Context");
+        fprintf(stderr, "Failed to create Window!");
+		glfwTerminate();
         return EXIT_FAILURE;
     }
 
     // Create Context and Load OpenGL Functions
     glfwMakeContextCurrent(mWindow);
-    gladLoadGL();
+
+	if(!gladLoadGL()) {
+		printf("Failed to load OpenGL functions!");
+        glfwDestroyWindow(mWindow);
+        glfwTerminate();
+		return EXIT_FAILURE;
+	};
+
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
     // Rendering Loop
@@ -36,13 +47,12 @@ int main(int argc, char * argv[]) {
         if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(mWindow, 1);
 
-        // Background Fill Color
         glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
         glfwPollEvents();
-    }   glfwTerminate();
+    }
+	glfwTerminate();
     return EXIT_SUCCESS;
 }
